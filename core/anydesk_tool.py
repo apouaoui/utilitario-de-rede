580f93331a478147b4a8dca2667f9a4daa825208
Arthur <arthurpassos.full@gmail.com>
2026-07-03 09:57:11 -0400

    Also delete leftover AnyDesk MSI installers when clearing AnyDesk data

--- core/anydesk_tool.py
+++ core/anydesk_tool.py
@@ -15,6 +15,25 @@ def _kill_anydesk():
     )
 
 
+def _find_msi_installers() -> list:
+    user_profile = os.environ.get("USERPROFILE", "")
+    search_dirs = [
+        os.path.join(user_profile, "Downloads"),
+        os.path.join(user_profile, "Desktop"),
+        os.environ.get("TEMP", ""),
+        os.path.join(os.environ.get("PROGRAMDATA", r"C:\ProgramData"), "AnyDesk"),
+    ]
+
+    found = []
+    for directory in search_dirs:
+        if not directory or not os.path.isdir(directory):
+            continue
+        for name in os.listdir(directory):
+            if name.lower().endswith(".msi") and "anydesk" in name.lower():
+                found.append(os.path.join(directory, name))
+    return found
+
+
 def clear_anydesk_data() -> list:
     _kill_anydesk()
     time.sleep(1)
@@ -34,5 +53,9 @@ def clear_anydesk_data() -> list:
         if os.path.isdir(path):
             shutil.rmtree(path)
             cleared.append(path)
+
+    for msi_path in _find_msi_installers():
+        os.remove(msi_path)
+        cleared.append(msi_path)
 
     return cleared
